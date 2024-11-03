0429682eafd16f77106504846a7755f1983a8ac0
cpekin <cpekin11@gmail.com>
2024-11-03 15:53:08 -0700

    new

--- hello.java
+++ hello.java
@@ -3,4 +3,19 @@ import java.time.LocalTime;
 
 public class HelloWorld {
 
+     public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.displayProject();
+        helloWorld.displayTime();
+       
+    }
+    public void displayProject() {
+        System.out.println("Hello, World!");
+        System.out.println("This is some test code for skills module");
+    }
+     public void displayTime() {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Current Time: " + currentTime);
+    }
+
 }
